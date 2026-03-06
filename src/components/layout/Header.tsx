aae464024b8a93b4f445fe7a6ef023aedf187f2f
Selva-imagiNET <selva@imaginetventures.com>
2026-03-06 19:22:56 +0530

    changes made

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -275,7 +275,7 @@ const Header = ({ settings }: HeaderProps) => {
                 alt="Logo"
                 width={160}
                 height={60}
-                className="h-12 w-auto object-contain"
+                className="h-12 w-auto object-contain mt-2"
               />
               <button
                 onClick={() => setMobileOpen(false)}
